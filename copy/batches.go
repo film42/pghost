15981c7be6336f7c25a80a93d9d40f160f4789ec
Garrett Thornburg <film42@gmail.com>
2020-02-04 22:05:36 -0500

    Few tests and scripts around batch copying things + logical rep

---
+++ copy/batches.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"context"
+)
+
+type W struct {
+	io.Writer
+}
+
+func (t *W) Write(b []byte) (int, error) {
+	n, err := t.Writer.Write(b)
+	fmt.Println("Wrote n bytes:", n)
+	return n, err
+}
+
+type R struct {
+	io.Reader
+}
+
+func (t *R) Read(b []byte) (int, error) {
+	n, err := t.Reader.Read(b)
+	// fmt.Println("Read n bytes:", n)
+	return n, err
+}
+
+// The idea here is that we can scan through an index in the most cheap and naive way
+// possible: Walk the ID as a simulated keyset pagination scan.
+// Start with the min(id) and walk by 1,000,000 to max(id).
+// From this point, the replication merge tool should take over.
+// Will this work? Idk. Let's find out.
+func copyIdRange(srcTable, destTable string, minId, batchSize int64) error {
+	// Create the COPY TO.
+	copyToQuery := fmt.Sprintf("COPY (SELECT * FROM %s WHERE id >= %d AND id < (%d + %d)) TO STDOUT WITH BINARY", srcTable, minId, minId, batchSize)
+	log.Println(copyToQuery)
+	copyToCmd := exec.Command("psql", "-U", "postgres", "-c", copyToQuery)
+	copyToCmd.Stderr = os.Stderr
+
+	// Create the COPY FROM.
+	copyFromQuery := fmt.Sprintf("COPY %s FROM STDIN WITH BINARY", destTable)
+	log.Println(copyFromQuery)
+	copyFromCmd := exec.Command("psql", "-U", "postgres", "-c", copyFromQuery)
+	copyFromCmd.Stderr = os.Stderr
+
+	// Wire the COPY TO to point at the COPY FROM.
+	copyToCmdStdout, err := copyToCmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	// defer copyToCmdStdout.Close()
+	copyFromCmd.Stdin = copyToCmdStdout
+
+	// Run the processes to completion.
+	if err = copyFromCmd.Start(); err != nil {
+		return err
+	}
+	if err = copyToCmd.Start(); err != nil {
+		return err
+	}
+
+	if err = copyToCmd.Wait(); err != nil {
+		return err
+	}
+	if err = copyFromCmd.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type BatchCopyBoy struct {
+	minId            int64
+	maxId            int64
+	SourceTable      string
+	DestinationTable string
+}
+
+func (cb *BatchCopyBoy) discoverIdRange() error {
+	cmd := exec.Command("psql", "-U", "postgres", "-Atc", fmt.Sprintf("SELECT MIN(id) FROM %s", cb.SourceTable))
+	n, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	min, err := strconv.ParseInt(strings.TrimSpace(string(n)), 0, 64)
+	if err != nil {
+		return err
+	}
+	cb.minId = min
+
+	cmd = exec.Command("psql", "-U", "postgres", "-Atc", fmt.Sprintf("SELECT MAX(id) FROM %s", cb.SourceTable))
+	n, err = cmd.Output()
+	if err != nil {
+		return err
+	}
+	max, err := strconv.ParseInt(strings.TrimSpace(string(n)), 0, 64)
+	if err != nil {
+		return err
+	}
+	cb.maxId = max
+	return nil
+}
+
+func (cb *BatchCopyBoy) CopyInBatches(batchSize int64, workers int) error {
+	err := cb.discoverIdRange()
+	if err != nil {
+		return err
+	}
+
+	// DEBUG: Print range
+	log.Println("Min:", cb.minId, "Max:", cb.maxId)
+	errorsChan := make(chan error, 100)
+	// This needs to be blocking so we can know all work has been handed off.
+	pendingWorkChan := make(chan int64)
+	wg := sync.WaitGroup{}
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	// Spawn workers.
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					wg.Done()
+					return
+				case nextId := <-pendingWorkChan:
+					err := copyIdRange(cb.SourceTable, cb.DestinationTable, nextId, batchSize)
+					if err != nil {
+						log.Println("WARNING!!! nextId:", nextId, "Found err while copying:", err)
+						errorsChan <- err
+					}
+				}
+			}
+		}()
+	}
+
+	// Send work with blocking chan.
+	nextId := cb.minId
+	for {
+		if nextId > cb.maxId {
+			break
+		}
+
+		select {
+		case pendingWorkChan <- nextId:
+		case err := <-errorsChan:
+			cancelFunc()
+			return err
+		}
+		nextId += batchSize
+	}
+
+	// Await.
+	cancelFunc()
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	cb := &BatchCopyBoy{
+		SourceTable:      "yolos",
+		DestinationTable: "yolos2",
+	}
+	// ./copy  2.34s user 1.38s system 19% cpu 19.259 total
+	fmt.Println(cb.CopyInBatches(100000, 10))
+
+	// ./copy  2.23s user 1.73s system 11% cpu 34.379 total
+	// fmt.Println(cb.CopyInBatches(100000, 2))
+
+	// ./copy  1.10s user 1.00s system 4% cpu 45.838 total
+	// fmt.Println(cb.CopyInBatches(100000000, 1))
+	fmt.Println("Done!")
+}
---
+++ main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"encoding/binary"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pglogrepl"
+	"github.com/jackc/pgproto3"
+)
+
+func main() {
+	conn, err := pgconn.Connect(context.Background(), "dbname=postgres replication=database")
+	if err != nil {
+		log.Fatalln("failed to connect to PostgreSQL server:", err)
+	}
+	defer conn.Close(context.Background())
+
+	sysident, err := pglogrepl.IdentifySystem(context.Background(), conn)
+	if err != nil {
+		log.Fatalln("IdentifySystem failed:", err)
+	}
+	log.Println("SystemID:", sysident.SystemID, "Timeline:", sysident.Timeline, "XLogPos:", sysident.XLogPos, "DBName:", sysident.DBName)
+
+	slotName := "yolo12300000xyz"
+
+	_, err = pglogrepl.CreateReplicationSlot(context.Background(), conn, slotName, "pgoutput", pglogrepl.CreateReplicationSlotOptions{Temporary: true})
+	if err != nil {
+		log.Fatalln("CreateReplicationSlot failed:", err)
+	}
+	log.Println("Created temporary replication slot:", slotName)
+
+	time.Sleep(time.Second * 5)
+
+	err = pglogrepl.StartReplication(context.Background(), conn, slotName, sysident.XLogPos, pglogrepl.StartReplicationOptions{}, "(proto_version '1', publication_names 'my_pub')")
+	if err != nil {
+		log.Fatalln("StartReplication failed:", err)
+	}
+	log.Println("Logical replication started on slot", slotName)
+
+	clientXLogPos := sysident.XLogPos
+	standbyMessageTimeout := time.Second * 10
+	nextStandbyMessageDeadline := time.Now().Add(standbyMessageTimeout)
+
+	for {
+		if time.Now().After(nextStandbyMessageDeadline) {
+			err = pglogrepl.SendStandbyStatusUpdate(context.Background(), conn, pglogrepl.StandbyStatusUpdate{WALWritePosition: clientXLogPos})
+			if err != nil {
+				log.Fatalln("SendStandbyStatusUpdate failed:", err)
+			}
+			log.Println("Sent Standby status message")
+			nextStandbyMessageDeadline = time.Now().Add(standbyMessageTimeout)
+		}
+
+		ctx, cancel := context.WithDeadline(context.Background(), nextStandbyMessageDeadline)
+		msg, err := conn.ReceiveMessage(ctx)
+		cancel()
+		if err != nil {
+			if pgconn.Timeout(err) {
+				continue
+			}
+			log.Fatalln("ReceiveMessage failed:", err)
+		}
+
+		switch msg := msg.(type) {
+		case *pgproto3.CopyData:
+			switch msg.Data[0] {
+			case pglogrepl.PrimaryKeepaliveMessageByteID:
+				pkm, err := pglogrepl.ParsePrimaryKeepaliveMessage(msg.Data[1:])
+				if err != nil {
+					log.Fatalln("ParsePrimaryKeepaliveMessage failed:", err)
+				}
+				log.Println("Primary Keepalive Message =>", "ServerWALEnd:", pkm.ServerWALEnd, "ServerTime:", pkm.ServerTime, "ReplyRequested:", pkm.ReplyRequested)
+
+				if pkm.ReplyRequested {
+					nextStandbyMessageDeadline = time.Time{}
+				}
+
+			case pglogrepl.XLogDataByteID:
+				xld, err := pglogrepl.ParseXLogData(msg.Data[1:])
+				if err != nil {
+					log.Fatalln("ParseXLogData failed:", err)
+				}
+				log.Println("XLogData =>", "WALStart", xld.WALStart, "ServerWALEnd", xld.ServerWALEnd, "ServerTime:", xld.ServerTime, "WALData", tryParse(xld.WALData))
+
+				clientXLogPos = xld.WALStart + pglogrepl.LSN(len(xld.WALData))
+			default:
+				log.Println("Something else:", msg.Data[0])
+			}
+		default:
+			log.Printf("Received unexpected message: %#v\n", msg)
+		}
+
+	}
+}
+
+func tryParse(data []byte) string {
+	log.Println("FIRST BYTE:", data[0], "STR:", string(data[0]))
+
+	switch data[0] {
+	case 'B':
+		log.Println("BEGIN")
+		log.Println("Final LSN of txn:", pglogrepl.LSN(binary.BigEndian.Uint64(data[1:9])).String())
+	case 'R':
+		cur := 1
+		log.Println("RELATION")
+		log.Println("Rel ID:", binary.BigEndian.Uint32(data[1:5]))
+		str, n := parseString(data[5:])
+		cur = n + 5
+		log.Println("Namespace:", str)
+		str, n = parseString(data[cur:])
+		cur += n
+		log.Println("Namespace:", str)
+		log.Println("relreplident:", string(uint8(data[cur])))
+		cur += 1
+		numCols := binary.BigEndian.Uint16(data[cur:cur+2])
+		cur += 2
+		log.Println("Num cols:", numCols)
+
+		for i := uint16(0); i < numCols; i++ {
+			log.Println("== Column", i+1, "==")
+			log.Println("Col part of key:", uint8(data[cur]))
+			cur++
+			str, n = parseString(data[cur:])
+			cur += n
+			log.Println("Col name:", str)
+			log.Println("Col type ID:", binary.BigEndian.Uint32(data[cur:cur+5]))
+			cur += 5
+			log.Println("atttypmod:", binary.BigEndian.Uint32(data[cur:cur+5]))
+			cur += 5
+		}
+
+	case 'I':
+		log.Println("INSERT")
+		log.Println("Rel ID:", binary.BigEndian.Uint32(data[1:5]))
+		log.Println("New tuple?:", string(data[5]))
+
+		cur := 6
+		numCols := binary.BigEndian.Uint16(data[cur:cur+2])
+		cur += 2
+
+		for i := uint16(0); i < numCols; i++ {
+			log.Println("== TupleData: == ")
+
+			switch data[cur] {
+			case 'n':
+			case 'u':
+			case 't':
+				cur++
+				vsize := binary.BigEndian.Uint32(data[cur:cur+4])
+				cur += 4
+				log.Println("  Tup Size:", vsize)
+				b := data[cur:cur+int(vsize)]
+				log.Println("  Tup Data:", b, "str:", string(b))
+				cur += int(vsize)
+			}
+
+		}
+
+	case 'C':
+		log.Println("COMMIT")
+		log.Println("LSN of commit:", pglogrepl.LSN(binary.BigEndian.Uint64(data[2:10])).String())
+		log.Println("LSN of txn:", pglogrepl.LSN(binary.BigEndian.Uint64(data[10:18])).String())
+	}
+
+	return string(data)
+}
+
+func parseString(data []byte) (string, int) {
+	s := 0
+	for {
+		if s > len(data) {
+			return "", -1
+		}
+		if data[s] == byte(0) {
+			return string(data[:s]), s+1
+		}
+		s++
+	}
+}
