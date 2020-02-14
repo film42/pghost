7238fc4e30a209ddcca7ed5e75b87a2f1dee6714
Garrett Thornburg <film42@gmail.com>
2020-02-14 14:22:47 -0500

    Remove the "only send status with writes" because it breaks healthchecks

--- replication/replication.go
+++ replication/replication.go
@@ -68,8 +68,6 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 		return err
 	}
 
-	var hadWritesSinceLastCommit bool
-
 	// Preload the statements to be applied.
 	statements := make([]string, 0, 1024)
 
@@ -77,8 +75,6 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 	// NOTE: This is best effor, so it's ok if we go beyond the checkpoint LSN a little bit
 	// since the main goal is to transition to the builtin wal sender worker.
 	for lastAckedLSN < checkpointLSN {
-		var shouldAckKnownLSN bool
-
 		// NOTE: We should tick at least once every 10 seconds as a heart beat.
 		rcvCtx, cancelFunc := context.WithTimeout(ctx, time.Second*10)
 
@@ -87,8 +83,7 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 		cancelFunc()
 		switch {
 		case pgconn.Timeout(err):
-			// Time to send a heartbeat.
-			shouldAckKnownLSN = true
+			// Send a status update.
 		case err != nil:
 			return err
 		}
@@ -96,7 +91,7 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 		// Handle the message
 		switch msg := msg.(type) {
 		case nil:
-			// timeout triggered, continue...
+			// Timeout triggered, continue so we can send a heartbeat status update.
 		case *pgproto3.CopyData:
 			switch msg.Data[0] {
 			case pglogrepl.PrimaryKeepaliveMessageByteID:
@@ -107,10 +102,6 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 				// Update the latest ack LSN if it's older than the server wal end.
 				if pkm.ServerWALEnd > lastAckedLSN {
 					lastAckedLSN = pkm.ServerWALEnd
-				}
-				if pkm.ReplyRequested {
-					// Signal we should ACK our completed LSN.
-					shouldAckKnownLSN = true
 				}
 
 			case pglogrepl.XLogDataByteID:
@@ -130,7 +121,6 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 				case *pgoutput.Begin:
 					// Reset statement list.
 					statements = statements[:0]
-					hadWritesSinceLastCommit = false
 					sql, err := lr.Handler.BeginToSql(v)
 					if err != nil {
 						return err
@@ -138,30 +128,24 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 					statements = append(statements, sql)
 				case *pgoutput.Commit:
 					sql, err := lr.Handler.CommitToSql(v)
-					// Signal that an ACK should be sent back to the DB.
-					// Only do it if there were writes. This cuts down on the millions of txns that happen.
-					shouldAckKnownLSN = hadWritesSinceLastCommit
 					lastAckedLSN = pglogrepl.LSN(v.TransactionLSN)
 					if err != nil {
 						return err
 					}
 					statements = append(statements, sql)
 				case *pgoutput.Delete:
-					hadWritesSinceLastCommit = true
 					sql, err := lr.Handler.DeleteToSql(v)
 					if err != nil {
 						return err
 					}
 					statements = append(statements, sql)
 				case *pgoutput.Insert:
-					hadWritesSinceLastCommit = true
 					sql, err := lr.Handler.InsertToSql(v)
 					if err != nil {
 						return err
 					}
 					statements = append(statements, sql)
 				case *pgoutput.Update:
-					hadWritesSinceLastCommit = true
 					sql, err := lr.Handler.UpdateToSql(v)
 					if err != nil {
 						return err
@@ -175,6 +159,7 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 				if err != nil {
 					return err
 				}
+
 			default:
 				return errors.New(fmt.Sprintf("error: received unknown replication message type: %v", msg.Data[0]))
 			}
@@ -182,28 +167,26 @@ func (lr *LogicalReplicator) ReplicateUpToCheckpoint(ctx context.Context, name s
 			return errors.New(fmt.Sprintf("error: did not received copy data, received: %+v", msg))
 		}
 
-		// ACK if requested
-		if shouldAckKnownLSN {
-			// Only apply statements if we have more than BEGIN; COMMIT;
-			if len(statements) > 2 {
-				err = lr.SqlApplier(ctx, statements)
-				if err != nil {
-					return err
-				}
-			}
-
-			// Only ACK if the sql applier finished OK.
-			err = pglogrepl.SendStandbyStatusUpdate(ctx, lr.Conn,
-				pglogrepl.StandbyStatusUpdate{
-					WALApplyPosition: lastAckedLSN,
-					WALFlushPosition: lastAckedLSN,
-					WALWritePosition: lastAckedLSN,
-				})
+		// Only apply statements if we have more than BEGIN; COMMIT;
+		if len(statements) > 2 {
+			err = lr.SqlApplier(ctx, statements)
 			if err != nil {
 				return err
 			}
-			log.Println("LSN Status Update:", lastAckedLSN)
 		}
+
+		// Only ACK if the sql applier finished OK.
+		err = pglogrepl.SendStandbyStatusUpdate(ctx, lr.Conn,
+			pglogrepl.StandbyStatusUpdate{
+				WALApplyPosition: lastAckedLSN,
+				WALFlushPosition: lastAckedLSN,
+				WALWritePosition: lastAckedLSN,
+			})
+		if err != nil {
+			return err
+		}
+		log.Println("LSN Status Update:", lastAckedLSN)
+
 	}
 
 	// If we've gotten to this point then we know that we have handled all changes up to the checkpoint LSN.
